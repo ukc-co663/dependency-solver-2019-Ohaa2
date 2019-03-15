9fc9db1f65a35989f82da978d312059d46f4c895
Obaid <ohaa2@kent.ac.uk>
2019-03-15 08:38:59 +0000

    Version 3.5

--- src/depsolver/Main.java
+++ src/depsolver/Main.java
@@ -2,155 +2,134 @@ package depsolver;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 
+@SuppressWarnings("restriction")
 public class Main {
 
 	public Main() {
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ScriptException {
+		long t1 = new Date().getTime();
 		TypeReference<List<Package>> repoType = new TypeReference<List<Package>>() {
 		};
-		List<Package> repositories = JSON.parseObject(Utils.readFile(args[0]), repoType);
-		Collections.sort(repositories, Collections.reverseOrder());
-		// Map<String, Package> repositories = new HashMap<String, Package>();
-		// for (Package package1 : repos) {
-		// repositories.put(package1.getName() + "=" + package1.getVersion(), package1);
-		// }
+		List<Package> repos = JSON.parseObject(Utils.readFile(args[0]), repoType);
+		Map<String, Map<String, Package>> repositories = new HashMap<String, Map<String, Package>>();
+		for (Package package1 : repos) {
+			if (repositories.get(package1.getName()) == null) {
+				repositories.put(package1.getName(), new HashMap<String, Package>());
+			}
+
+			repositories.get(package1.getName()).put(package1.getVersion(), package1);
+		}
 
 		TypeReference<List<String>> strListType = new TypeReference<List<String>>() {
 		};
 
-		List<String> initialPackages = JSON.parseObject(Utils.readFile(args[1]), strListType);
+		List<String> inits = JSON.parseObject(Utils.readFile(args[1]), strListType);
+		Map<String, List<String>> initials = new HashMap<String, List<String>>();
+		for (String initial : inits) {
+			String[] decomposedInitial = Utils.decomposeConstraint(initial);
+			if (initials.get(decomposedInitial[0]) == null) {
+				initials.put(decomposedInitial[0], new ArrayList<String>());
+			}
+			initials.get(decomposedInitial[0]).add(decomposedInitial[2]);
+		}
 
 		List<String> allConstraints = JSON.parseObject(Utils.readFile(args[2]), strListType);
 
-		boolean installed = false;
-		Result finalResult = new Result(0L);
-		// do {
+		Result lowestCostResult = new Result(0L);
 		for (String constraint : allConstraints) {
-			String[] decompsedConstraint = Utils.decomposeConstraint(constraint.substring(1), repositories);
+			String[] decompsedConstraint = Utils.decomposeConstraint(constraint.substring(1));
 
 			switch (constraint.substring(0, 1)) {
 			case "+":
-				Result wildCardResult = new Result(Long.MAX_VALUE);
-				for (Package package1 : repositories) {
-					boolean isMatched = false;
-					Result result = new Result(0L);
-					if (package1.getName().equals(decompsedConstraint[0])) {
-						switch (decompsedConstraint[1]) {
-						case ">=":
-							if (package1.getVersion().compareTo(decompsedConstraint[2]) >= 0) {
-								isMatched = true;
+				List<Package> installList = new ArrayList<>();
+				if (decompsedConstraint[1].equals("=")) {
+					installList.add(repositories.get(decompsedConstraint[0]).get(decompsedConstraint[2]));
+				} else {
+					ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+					ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("JavaScript");
+					for (String version : repositories.get(decompsedConstraint[0]).keySet()) {
+						if (decompsedConstraint[1].equals("")) {
+							installList.add(repositories.get(decompsedConstraint[0]).get(version));
+						} else {
+							if (Boolean
+									.valueOf(String.valueOf(scriptEngine.eval(version.compareTo(decompsedConstraint[2])
+											+ " " + decompsedConstraint[1] + " 0")))) {
+								installList.add(repositories.get(decompsedConstraint[0]).get(version));
 							}
-							break;
-						case ">":
-							if (package1.getVersion().compareTo(decompsedConstraint[2]) > 0) {
-								isMatched = true;
-							}
-							break;
-						case "<=":
-							if (package1.getVersion().compareTo(decompsedConstraint[2]) <= 0) {
-								isMatched = true;
-							}
-							break;
-						case "<":
-							if (package1.getVersion().compareTo(decompsedConstraint[2]) < 0) {
-								isMatched = true;
-							}
-							break;
-						case "=":
-							if (package1.getVersion().compareTo(decompsedConstraint[2]) == 0) {
-								isMatched = true;
-							}
-							break;
-						case "":
-							isMatched = true;
-							break;
-						}
-
-						if (isMatched && (installed = Utils.installPackage(result, package1, initialPackages,
-								repositories, new ArrayList<String>()))) {
-							if (decompsedConstraint[1].equals("")) {
-								if (wildCardResult.getCost() > result.getCost()) {
-									if (wildCardResult.getCost() != Long.MAX_VALUE) {
-										finalResult.setCost(finalResult.getCost() - wildCardResult.getCost());
-										finalResult.getCommands().removeAll(wildCardResult.getCommands());
-									}
-									wildCardResult = result;
-								} else {
-									result = new Result(0L);
-								}
-							}
-							finalResult.setCost(finalResult.getCost() + result.getCost());
-							finalResult.getCommands().addAll(result.getCommands());
 						}
 					}
 				}
+
+				Result minimumCostResult = new Result(Long.MAX_VALUE);
+				for (Package package1 : installList) {
+					Result result = new Result(0L);
+					if (Utils.installPackage(result, package1, initials, repositories, new ArrayList<String>())) {
+						if (minimumCostResult.getCost() > result.getCost()) {
+							for (String cmd : result.getCommands()) {
+								String[] decomposedCmd = Utils.decomposeConstraint(cmd);
+								if (initials.get(decomposedCmd[0]) == null) {
+									initials.put(decomposedCmd[0], new ArrayList<String>());
+								}
+								initials.get(decomposedCmd[0]).add(decomposedCmd[2]);
+							}
+							minimumCostResult.setCost(result.getCost());
+							minimumCostResult.setCommands(result.getCommands());
+						}
+					}
+				}
+
+				lowestCostResult.setCost(lowestCostResult.getCost() + minimumCostResult.getCost());
+				lowestCostResult.getCommands().addAll(minimumCostResult.getCommands());
+
 				break;
 			case "-":
-				for (Package pack : repositories) {
-					boolean matchFound = false;
-					Result result = new Result(0L);
-					if (pack.getName().equals(decompsedConstraint[0])) {
-						switch (decompsedConstraint[1]) {
-						case ">=":
-							if (pack.getVersion().compareTo(decompsedConstraint[2]) >= 0) {
-								matchFound = true;
+				List<Package> uninstallList = new ArrayList<>();
+				if (decompsedConstraint[1].equals("=")) {
+					uninstallList.add(repositories.get(decompsedConstraint[0]).get(decompsedConstraint[2]));
+				} else {
+					ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+					ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("JavaScript");
+					for (String version : repositories.get(decompsedConstraint[0]).keySet()) {
+						if (decompsedConstraint[1].equals("")) {
+							uninstallList.add(repositories.get(decompsedConstraint[0]).get(version));
+						} else {
+							if (Boolean
+									.valueOf(String.valueOf(scriptEngine.eval(version.compareTo(decompsedConstraint[2])
+											+ " " + decompsedConstraint[1] + " 0")))) {
+								uninstallList.add(repositories.get(decompsedConstraint[0]).get(version));
 							}
-							break;
-						case ">":
-							if (pack.getVersion().compareTo(decompsedConstraint[2]) > 0) {
-								matchFound = true;
-							}
-							break;
-						case "<=":
-							if (pack.getVersion().compareTo(decompsedConstraint[2]) <= 0) {
-								matchFound = true;
-							}
-							break;
-						case "<":
-							if (pack.getVersion().compareTo(decompsedConstraint[2]) < 0) {
-								matchFound = true;
-							}
-							break;
-						case "=":
-							if (pack.getVersion().compareTo(decompsedConstraint[2]) == 0) {
-								matchFound = true;
-							}
-							break;
-						case "":
-							matchFound = true;
-							break;
-						}
-
-						if (matchFound && Utils.uninstallPackage(result, pack, initialPackages, repositories)) {
-							finalResult.setCost(finalResult.getCost() + result.getCost());
-							finalResult.getCommands().addAll(result.getCommands());
 						}
 					}
 				}
+
+				for (Package package1 : uninstallList) {
+					Result result = new Result(0L);
+					if (Utils.uninstallPackage(result, package1, initials)) {
+						lowestCostResult.setCost(lowestCostResult.getCost() + result.getCost());
+						lowestCostResult.getCommands().addAll(result.getCommands());
+					}
+				}
+
 				break;
 			}
 		}
-		// if (!installed) {
-		// finalResult.setCost(Utils.uninstallCost);
-		// finalResult.setCommands(new LinkedHashSet<String>());
-		// finalResult.getCommands().add("-" + initialPackages.get(0));
-		// initialPackages.remove(initialPackages.get(0));
-		// }
-		// } while (!installed);
 
-		// Utils.writeFile(args[0].substring(0, args[0].lastIndexOf(File.separator) + 1)
-		// + "commands.json",
-		// JSON.toJSONString(finalResult.getCommands(), true));
-
-		//System.out.println(finalResult.getCost());
-		System.out.println(JSON.toJSONString(finalResult.getCommands()));
+		// System.out.println(lowestCostResult.getCost());
+		System.out.println(JSON.toJSONString(lowestCostResult.getCommands(), true));
+		// System.out.println(new Date().getTime() - t1);
 	}
 }
--- src/depsolver/Utils.java
+++ src/depsolver/Utils.java
@@ -2,14 +2,22 @@ package depsolver;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+@SuppressWarnings("restriction")
 public class Utils {
 
 	public static Long uninstallCost = 1000000L;
-	public static String[] allOperators = new String[] { ">=", ">", "<=", "<", "=" };
 
 	public Utils() {
 	}
@@ -28,76 +36,51 @@ public class Utils {
 		}
 	}
 
-	public static void writeFile(String filename, String json) throws IOException {
-		FileWriter fw = null;
-		try {
-			fw = new FileWriter(filename);
-			fw.write(json);
-		} finally {
-			if (fw != null) {
-				fw.close();
-			}
-		}
-	}
+	public static String[] decomposeConstraint(String constraint) {
+		String[] constraintArray = { "", "", "" };
 
-	public static String[] decomposeConstraint(String constraint, List<Package> repositories) {
-		String localConstraint = new String(constraint);
-		String[] constraintArray = new String[3];
-		for (Package package1 : repositories) {
-			if (localConstraint.contains(package1.getName())) {
-				constraintArray[0] = package1.getName();
-				break;
-			} else
-				constraintArray[0] = "";
+		Pattern pattern = Pattern.compile("(.+?)(?=(>=)|(>)|(<=)|(<)|(=))");
+		Matcher matcher = pattern.matcher(constraint);
+
+		if (matcher.find()) {
+			constraintArray[0] = matcher.group();
+
+			pattern = Pattern.compile("((>=)|(>)|(<=)|(<)|(=))+?");
+			matcher = pattern.matcher(constraint);
+			matcher.find();
+
+			constraintArray[1] = matcher.group();
+
+			constraintArray[2] = constraint.substring(matcher.end());
+		} else {
+			constraintArray[0] = constraint;
 		}
-		for (String operator : allOperators) {
-			if ((localConstraint.indexOf(operator) != -1)) {
-				constraintArray[1] = operator;
-				break;
-			} else
-				constraintArray[1] = "";
-		}
-		constraintArray[2] = localConstraint.replace(constraintArray[0] + constraintArray[1], "");
+
 		return constraintArray;
 	}
 
-	public static boolean conflictsExist(Package pack, List<String> installedPackages, List<Package> repositories) {
-		if (pack.getConflicts() == null || pack.getConflicts().isEmpty()) {
+	public static boolean conflictsExist(Package package1, Map<String, List<String>> installedPackages)
+			throws ScriptException {
+		if (package1.getConflicts() == null || package1.getConflicts().isEmpty()) {
 			return false;
 		} else {
-			for (String conflict : pack.getConflicts()) {
-				String[] decompsedConflict = decomposeConstraint(conflict, repositories);
-				for (String installed : installedPackages) {
-					String[] decompsedInstalled = decomposeConstraint(installed, repositories);
-					if (decompsedInstalled[0].equals(decompsedConflict[0])) {
-						switch (decompsedConflict[1]) {
-						case ">=":
-							if (decompsedInstalled[2].compareTo(decompsedConflict[2]) >= 0) {
-								return true;
-							}
-							break;
-						case ">":
-							if (decompsedInstalled[2].compareTo(decompsedConflict[2]) > 0) {
-								return true;
-							}
-							break;
-						case "<=":
-							if (decompsedInstalled[2].compareTo(decompsedConflict[2]) <= 0) {
-								return true;
-							}
-							break;
-						case "<":
-							if (decompsedInstalled[2].compareTo(decompsedConflict[2]) < 0) {
-								return true;
-							}
-							break;
-						case "=":
-							if (decompsedInstalled[2].compareTo(decompsedConflict[2]) == 0) {
-								return true;
-							}
-							break;
-						case "":
+			ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+			ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("JavaScript");
+			for (String conflict : package1.getConflicts()) {
+				String[] decompsedConflict = decomposeConstraint(conflict);
+				if (installedPackages.get(decompsedConflict[0]) != null) {
+					if (decompsedConflict[1].equals("")) {
+						return true;
+					} else if (decompsedConflict[1].equals("=")) {
+						if (installedPackages.get(decompsedConflict[0]).contains(decompsedConflict[2])) {
 							return true;
+						}
+					} else {
+						for (String version : installedPackages.get(decompsedConflict[0])) {
+							if (Boolean.valueOf(String.valueOf(scriptEngine.eval(
+									version.compareTo(decompsedConflict[2]) + " " + decompsedConflict[1] + " 0")))) {
+								return true;
+							}
 						}
 					}
 				}
@@ -107,112 +90,153 @@ public class Utils {
 
 	}
 
-	public static boolean installPackage(Result result, Package pack, List<String> installedPackages,
-			List<Package> repositories, List<String> breakloop) {
-		if (breakloop.contains(pack.getName() + "=" + pack.getVersion())) {
+	public static boolean installPackage(Result result, Package package1, Map<String, List<String>> installedPackages,
+			Map<String, Map<String, Package>> repositories, List<String> path) throws ScriptException {
+		if (path.contains(package1.getName())) {
 			return false;
 		} else {
-			breakloop.add(pack.getName() + "=" + pack.getVersion());
+			path.add(package1.getName());
 		}
 
+		// System.out.println(path);
 		boolean installed = false;
-		if (pack != null && (pack.getDepends() == null || pack.getDepends().isEmpty())) {
-			if (!Utils.conflictsExist(pack, installedPackages, repositories)) {
-				result.setCost(result.getCost() + pack.getSize());
-				result.getCommands().add("+" + pack.getName() + "=" + pack.getVersion());
+		if (package1 != null && (package1.getDepends() == null || package1.getDepends().isEmpty())) {
+			if (!Utils.conflictsExist(package1, installedPackages)) {
+				result.setCost(result.getCost() + package1.getSize());
+				result.getCommands().add("+" + package1.getName() + "=" + package1.getVersion());
 				return true;
+			} else {
+				uninstallConflicts(result, package1, repositories, installedPackages);
 			}
 		}
 
 		Result conjuct = new Result(0L), disjunct = new Result(Long.MAX_VALUE);
-		for (int i = 0; i < pack.getDepends().size(); i++) {
+		List<String> localPath = new ArrayList<String>();
+		for (int i = 0; i < package1.getDepends().size(); i++) {
+			localPath.addAll(path);
+			if (i > 0) {
+				if (!localPath.isEmpty()) {
+					localPath.remove(localPath.size() - 1);
+				}
+			}
 			disjunct = new Result(Long.MAX_VALUE);
-			for (int j = 0; j < pack.getDepends().get(i).size(); j++) {
-				String[] decompsedDependant = Utils.decomposeConstraint(pack.getDepends().get(i).get(j), repositories);
-				for (Package package1 : repositories) {
-					boolean isMatched = false;
-					Result local = new Result(0L);
-					if (package1.getName().equals(decompsedDependant[0])) {
-						switch (decompsedDependant[1]) {
-						case ">=":
-							if (package1.getVersion().compareTo(decompsedDependant[2]) >= 0) {
-								isMatched = true;
-							}
-							break;
-						case ">":
-							if (package1.getVersion().compareTo(decompsedDependant[2]) > 0) {
-								isMatched = true;
-							}
-							break;
-						case "<=":
-							if (package1.getVersion().compareTo(decompsedDependant[2]) <= 0) {
-								isMatched = true;
-							}
-							break;
-						case "<":
-							if (package1.getVersion().compareTo(decompsedDependant[2]) < 0) {
-								isMatched = true;
-							}
-							break;
-						case "=":
-							if (package1.getVersion().compareTo(decompsedDependant[2]) == 0) {
-								isMatched = true;
-							}
-							break;
-						case "":
-							isMatched = true;
-							break;
-						}
-						if (isMatched && !Utils.conflictsExist(package1, installedPackages, repositories)) {
-							if ((installed = installPackage(local, package1, installedPackages, repositories,
-									breakloop))) {
-								if (disjunct.getCost() > local.getCost()) {
-									for (String cmd : disjunct.getCommands()) {
-										installedPackages.remove(cmd.substring(1));
-									}
-									disjunct = local;
-									for (String cmd : disjunct.getCommands()) {
-										installedPackages.add(cmd.substring(1));
-									}
+			for (int j = 0; j < package1.getDepends().get(i).size(); j++) {
+				String[] decompsedDependant = Utils.decomposeConstraint(package1.getDepends().get(i).get(j));
+
+				List<Package> installList = new ArrayList<>();
+				if (decompsedDependant[1].equals("=")) {
+					installList.add(repositories.get(decompsedDependant[0]).get(decompsedDependant[2]));
+				} else {
+					ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+					ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("JavaScript");
+					if (repositories.get(decompsedDependant[0]) != null) {
+						for (String version : repositories.get(decompsedDependant[0]).keySet()) {
+							if (decompsedDependant[1].equals("")) {
+								installList.add(repositories.get(decompsedDependant[0]).get(version));
+							} else {
+								if (Boolean.valueOf(
+										String.valueOf(scriptEngine.eval(version.compareTo(decompsedDependant[2]) + " "
+												+ decompsedDependant[1] + " 0")))) {
+									installList.add(repositories.get(decompsedDependant[0]).get(version));
 								}
 							}
-							breakloop.remove(breakloop.size() - 1);
 						}
 					}
 				}
+
+				Result oneDeptResult = new Result(Long.MAX_VALUE);
+				for (Package deptPackage : installList) {
+					Result local = new Result(0L);
+					Map<String, List<String>> existingPackages = new HashMap<String, List<String>>();
+					for (String key : installedPackages.keySet()) {
+						existingPackages.put(key, new ArrayList<String>());
+						for (String version : installedPackages.get(key)) {
+							existingPackages.get(key).add(version);
+						}
+					}
+					if (Utils.conflictsExist(deptPackage, installedPackages)) {
+						uninstallConflicts(local, deptPackage, repositories, existingPackages);
+					}
+					if (installed = Utils.installPackage(local, deptPackage, existingPackages, repositories, path)) {
+						if (oneDeptResult.getCost() > local.getCost()) {
+							oneDeptResult.setCost(local.getCost());
+							oneDeptResult.setCommands(local.getCommands());
+						}
+					}
+				}
+
+				if (disjunct.getCost() > oneDeptResult.getCost()) {
+					disjunct = oneDeptResult;
+				}
+				if (!path.isEmpty()) {
+					path.remove(path.size() - 1);
+				}
 			}
+
 			for (String cmd : disjunct.getCommands()) {
 				if (conjuct.getCommands().contains(cmd)) {
-					String[] command = decomposeConstraint(cmd.substring(1), repositories);
-					for (Package package1 : repositories) {
-						if (package1.getName().equals(command[0]) && package1.getVersion().equals(command[2])) {
-							disjunct.setCost(disjunct.getCost() - package1.getSize());
+					String[] command = decomposeConstraint(cmd.substring(1));
+					disjunct.setCost(disjunct.getCost() - repositories.get(command[0]).get(command[2]).getSize());
+				}
+			}
+
+			if (!disjunct.getCommands().isEmpty()) {
+				conjuct.setCost(conjuct.getCost() + disjunct.getCost());
+				conjuct.getCommands().addAll(disjunct.getCommands());
+			}
+		}
+
+		result.setCost(conjuct.getCost() + package1.getSize());
+		result.getCommands().addAll(conjuct.getCommands());
+		result.getCommands().add("+" + package1.getName() + "=" + package1.getVersion());
+
+		return installed;
+	}
+
+	public static boolean uninstallConflicts(Result result, Package package1,
+			Map<String, Map<String, Package>> repositories, Map<String, List<String>> installedPackages)
+			throws ScriptException {
+		if (package1 != null) {
+			for (String conflict : package1.getConflicts()) {
+				List<Package> uninstallList = new ArrayList<>();
+				String[] decompsedConflict = decomposeConstraint(conflict);
+				if (decompsedConflict[1].equals("=")) {
+					uninstallList.add(repositories.get(decompsedConflict[0]).get(decompsedConflict[2]));
+				} else {
+					ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+					ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("JavaScript");
+					for (String version : repositories.get(decompsedConflict[0]).keySet()) {
+						if (decompsedConflict[1].equals("")) {
+							uninstallList.add(repositories.get(decompsedConflict[0]).get(version));
+						} else {
+							if (Boolean.valueOf(String.valueOf(scriptEngine.eval(
+									version.compareTo(decompsedConflict[2]) + " " + decompsedConflict[1] + " 0")))) {
+								uninstallList.add(repositories.get(decompsedConflict[0]).get(version));
+							}
 						}
 					}
 				}
-			}
-			conjuct.setCost(conjuct.getCost() + disjunct.getCost());
-			conjuct.getCommands().addAll(disjunct.getCommands());
-		}
 
-		if (installed) {
-			result.setCost(conjuct.getCost() + pack.getSize());
-			result.getCommands().addAll(conjuct.getCommands());
-			result.getCommands().add("+" + pack.getName() + "=" + pack.getVersion());
+				for (Package package2 : uninstallList) {
+					Utils.uninstallPackage(result, package2, installedPackages);
+				}
+			}
 			return true;
-		} else
-			return false;
+		}
+		return false;
 	}
 
-	public static boolean uninstallPackage(Result result, Package pack, List<String> installedPackages,
-			List<Package> repositories) {
-		boolean uninstalled = false;
-		if (pack != null && (pack.getDepends() == null || pack.getDepends().isEmpty())) {
-			installedPackages.remove(pack.getName() + "=" + pack.getVersion());
+	public static boolean uninstallPackage(Result result, Package package1,
+			Map<String, List<String>> installedPackages) {
+		if (package1 != null) {
+			installedPackages.get(package1.getName()).remove(package1.getVersion());
+			if (installedPackages.get(package1.getName()).size() == 0) {
+				installedPackages.remove(package1.getName());
+			}
 			result.setCost(result.getCost() + Utils.uninstallCost);
-			result.getCommands().add("-" + pack.getName() + "=" + pack.getVersion());
+			result.getCommands().add("-" + package1.getName() + "=" + package1.getVersion());
 			return true;
 		}
-		return uninstalled;
+		return false;
 	}
 }
